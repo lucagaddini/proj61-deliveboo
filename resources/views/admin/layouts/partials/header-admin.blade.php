b3d364f0914b399805aac898ec554aabd76fa15f
lucagad <90861515+lucagad@users.noreply.github.com>
2022-08-26 17:04:41 +0200

    Creati file Blade parziali per Header ed Navbar laterale

---
+++ resources/views/admin/layouts/partials/header-admin.blade.php
@@ -0,0 +1,56 @@
+{{-- HEADER --}}
+        
+<nav class="navbar navbar_custom navbar-expand-md navbar-dark shadow-sm">
+  <div class="container-fluid">
+      <a class="navbar-brand" href="{{ url('/') }}">
+          <img src="{{asset('assets/img/logo.png')}}" width="40px" class="d-inline-block align-center" alt="DeliveBoo Logo">
+          DeliveBoo
+      </a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav mr-auto">
+          </ul>
+
+          <!-- Right Side Of Navbar -->
+          <ul class="navbar-nav ml-auto">
+              <!-- Authentication Links -->
+              @guest
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
+                  </li>
+                  @if (Route::has('register'))
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                      </li>
+                  @endif
+              @else
+                  <li class="nav-item dropdown">
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                          {{ Auth::user()->name }}
+                      </a>
+
+                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="{{ route('logout') }}"
+                              onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">
+                              {{ __('Logout') }}
+                          </a>
+
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                              @csrf
+                          </form>
+
+                      </div>
+                  </li>
+              @endguest
+          </ul>
+      </div>
+  </div>
+</nav>
+
+{{-- //HEADER --}}
