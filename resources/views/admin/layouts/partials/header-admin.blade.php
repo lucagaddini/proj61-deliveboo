176f036f57f11f489aad0b42dbf0836392d5edf1
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-08-29 19:22:33 +0200

    Versione Mobile della Navbar laterale

--- resources/views/admin/layouts/partials/header-admin.blade.php
+++ resources/views/admin/layouts/partials/header-admin.blade.php
@@ -34,7 +34,35 @@
                           {{ Auth::user()->name }}
                       </a>
 
+                      {{-- Sostituta della Navbar laterale su Mobile --}}
+                      <div class="mobile_navbar d-md-none">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.home') }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                    Dashboard <span class="sr-only">(current)</span>
+                                </a>
+                            </li>
+                
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
+                                    Ordini
+                                </a>
+                            </li>
+                
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.items.index') }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></svg>
+                                    Prodotti
+                                </a>
+                            </li>
+                        </ul>
+
+                      </div>
+
                       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
                           <a class="dropdown-item" href="{{ route('logout') }}"
                               onclick="event.preventDefault();
                                   document.getElementById('logout-form').submit();">
@@ -44,6 +72,8 @@
                           <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                               @csrf
                           </form>
+
+                          
 
                       </div>
                   </li>
