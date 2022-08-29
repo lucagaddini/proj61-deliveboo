f672a1d293833c6fd5ee9d020067c8d498156210
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-08-29 16:28:24 +0200

    Completata verifica Form registrazione lato client

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,13 +16,21 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome Ristorante') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus data-parsley-trigger="keyup">
+                                <input id="name" type="text" 
+                                class="form-control @error('name') is-invalid @enderror" 
+                                name="name" 
+                                value="{{ old('name') }}" 
+                                autocomplete="name" 
+                                autofocus
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                {{-- Utilizzo della libreria Parsley --}}
+                                required
+                                data-parsley-minlength="3"
+                                data-parsley-maxlength="255"
+                                data-parsley-trigger="keyup"
+                                {{-- // Parsley --}}
+                                >
+                    
                             </div>
                         </div>
 
@@ -31,13 +39,22 @@
                             <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo') }}</label>
 
                             <div class="col-md-6">
-                                <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address" autofocus>
+                                <input id="address" 
+                                type="text" 
+                                class="form-control @error('address') is-invalid @enderror" 
+                                name="address" 
+                                value="{{ old('address') }}" 
+                                autocomplete="address" 
+                                autofocus
 
-                                @error('address')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                {{-- Utilizzo della libreria Parsley --}}
+                                required
+                                data-parsley-minlength="3"
+                                data-parsley-maxlength="60"
+                                data-parsley-trigger="keyup"
+                                {{-- // Parsley --}}
+                                >
+
                             </div>
                         </div>
 
@@ -46,13 +63,22 @@
                             <label for="vat_number" class="col-md-4 col-form-label text-md-right">{{ __('P.Iva') }}</label>
 
                             <div class="col-md-6">
-                                <input id="vat_number" type="text" class="form-control @error('vat_number') is-invalid @enderror" name="vat_number" value="{{ old('vat_number') }}" required autocomplete="vat_number" autofocus>
+                                <input id="vat_number" 
+                                type="text" 
+                                class="form-control @error('vat_number') is-invalid @enderror" 
+                                name="vat_number" 
+                                value="{{ old('vat_number') }}" 
+                                autocomplete="vat_number" 
+                                autofocus
+                                
+                                {{-- Utilizzo della libreria Parsley --}}
+                                required
+                                data-parsley-minlength="11"
+                                data-parsley-maxlength="16"
+                                data-parsley-trigger="keyup"
+                                {{-- // Parsley --}}
+                                >
 
-                                @error('vat_number')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
 
@@ -61,13 +87,19 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" 
+                                type="email" 
+                                class="form-control @error('email') is-invalid @enderror" 
+                                name="email" value="{{ old('email') }}" 
+                                autocomplete="email"
+                                
+                                {{-- Utilizzo della libreria Parsley --}}
+                                required
+                                data-parsley-maxlength="255"
+                                data-parsley-trigger="keyup"
+                                {{-- // Parsley --}}
+                                >
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
 
@@ -76,13 +108,20 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" 
+                                type="password" 
+                                class="form-control @error('password') is-invalid @enderror" 
+                                name="password" 
+                                autocomplete="new-password"
+                                
+                                {{-- Utilizzo della libreria Parsley --}}
+                                required
+                                data-parsley-minlength="8"
+                                data-parsley-maxlength="255"
+                                data-parsley-trigger="keyup"
+                                {{-- // Parsley --}}
+                                >
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
 
@@ -91,14 +130,27 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" 
+                                type="password" 
+                                class="form-control" 
+                                name="password_confirmation" 
+                                autocomplete="new-password"
+                                
+                                {{-- Utilizzo della libreria Parsley --}}
+                                required
+                                data-parsley-equalto="#password"
+                                data-parsley-maxlength="255"
+                                data-parsley-trigger="keyup"
+                                {{-- // Parsley --}}
+                                
+                                >
                             </div>
                         </div>
 
 
                         {{-- Checkbox Categorie --}}
                         <div class="form-group row">
-                            <label for="categories" class="col-md-4 col-form-label text-md-right">{{ __('Seleziona le categorie') }} <br> (Scegli bene, non sarà possibile aggiornarle successivamente)</label>
+                            <label for="categories" class="col-md-4 col-form-label text-md-right">{{ __('Seleziona le categorie') }} <br> (Scegliere attentamente, non sarà possibile modificarle successivamente)</label>
 
                             <div class="col-md-6">
 
@@ -106,14 +158,21 @@
                                     $categories = \App\Category::all();
                                 @endphp
 
-                                <div class="form-check @error('categories') is-invalid @enderror required" data-parsley-mincheck="1">
+                                <div class="form-check @error('categories') is-invalid @enderror" >
 
-                                    @foreach ( $categories as $category )
+                                    @foreach ( $categories as $category)
 
                                     <div class="form-check">
                                         <input class="form-check-input"
                                             type="checkbox"
                                             name="categories[]"
+
+                                            {{-- Utilizzo della libreria Parsley per il checkbox ed assegnazione "required" all'ultimo elemento dell'array --}}
+                                            data-parsley-mincheck="1"
+                                            {{ ($category->id === count($categories) ) ? 'required' : '' }}
+                                            data-parsley-errors-container="#error-checkbox"
+                                            {{-- Fine utilizzo libreria --}}
+
                                             value="{{$category->id}}"
                                             id="{{'category-'.$category->id}}"
                                             {{ (is_array(old('categories')) && in_array($category->id, old('categories'))) ? ' checked' : '' }}
@@ -125,24 +184,24 @@
 
                                     @endforeach
 
+                                    {{-- Span contenente gli errori relativi al gruppo di checkbox --}}
+                                    <span id="error-checkbox">
+                                    </span>
+
                                 </div>
 
-                                @error('categories')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
 
-
+                        {{-- Bottone registrazione --}}
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn_custom">
                                     {{ __('Registrati') }}
                                 </button>
                             </div>
                         </div>
+
                     </form>
                 </div>
             </div>
