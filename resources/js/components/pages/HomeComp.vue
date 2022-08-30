ed189b4a4e5dcc306b9bef0345da39dd60320d23
Amanjit313 <amanjitsingh313@gmail.com>
2022-08-30 17:39:46 +0200

    categories-slider

--- resources/js/components/pages/HomeComp.vue
+++ resources/js/components/pages/HomeComp.vue
@@ -7,8 +7,6 @@
 <script>
 import HeroComp from "../partials/HeroComp.vue"
 
-
-
 export default {
     name: 'HomeComp',
     components:{
--- resources/js/components/partials/HeroComp.vue
+++ resources/js/components/partials/HeroComp.vue
@@ -1,33 +1,153 @@
 <template>
-<section class="herobanner">
-  <div class="container col-xxl-8 px-4 py-5">
-      <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
 
-        <div class="col-10 col-sm-8 col-lg-6">
+<div class="home">
+
+    <!-- Jumbotron -->
+    <section class="herobanner">
+        <div class="container col-xxl-8 px-4 py-5">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+
+                <div class="col-10 col-sm-8 col-lg-6">
+                </div>
+
+                <div class="col-lg-6">
+                <h1 class="display-5 fw-bold lh-1 mb-3">Il cibo che ami a Domicilio</h1>
+                <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate distinctio repudiandae perferendis quibusdam voluptas. Veritatis, enim rerum. Hic consectetur incidunt impedit, alias ratione tenetur ex praesentium nisi culpa vero aliquid?</p>
+                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+
+                    <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Inserisci il tipo di piatto" aria-label="Search">
+                    <button class="btn btn_custom_secondary" type="submit">Trova</button>
+                    </form>
+
+                </div>
+                </div>
+
+            </div>
         </div>
+    </section>
+    <!-- /Jumbotron -->
 
-        <div class="col-lg-6">
-          <h1 class="display-5 fw-bold lh-1 mb-3">Il cibo che ami a Domicilio</h1>
-          <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate distinctio repudiandae perferendis quibusdam voluptas. Veritatis, enim rerum. Hic consectetur incidunt impedit, alias ratione tenetur ex praesentium nisi culpa vero aliquid?</p>
-          <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Inserisci il tipo di piatto" aria-label="Search">
-              <button class="btn btn_custom_secondary" type="submit">Trova</button>
-            </form>
+    <!-- Categories and Restaurants -->
+    <section>
 
-          </div>
+        <!-- Categories -->
+        <div class="categories container">
+
+            <!-- Cat. Title -->
+            <div class="cat-title text-center">
+                <h1>Popular Categories</h1>
+                <p>Lorem ipsum dolor sit amet, adipisicing!</p>
+            </div>
+            <!-- /Cat. Title -->
+
+            <VueSlickCarousel v-bind='settings'>
+
+            <!-- Cat. Cards -->
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p> Pizza</p>
+            </div>
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p>Pizza</p>
+            </div>
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p>Pizza</p>
+            </div>
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p>Pizza</p>
+            </div>
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p>Pizza</p>
+            </div>
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p>Pizza</p>
+            </div>
+            <!-- /Cat. Cards -->
+
+            </VueSlickCarousel>
+
         </div>
+        <!-- /Categories -->
 
-      </div>
-    </div>
-  </section>
+        <!-- Restaurants -->
+        <div class="restaurants"></div>
+        <!-- /Restaurants -->
+
+    </section>
+    <!-- /Categories and Restaurants -->
+
+  </div>
 
 </template>
 
 <script>
+
+  import VueSlickCarousel from 'vue-slick-carousel';
+  import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+
 export default {
-    name: 'HeroComp'
+    name: 'HeroComp',
+    components: {
+        VueSlickCarousel
+    },
+
+    data() {
+      return {
+        settings: {
+            "arrows": true,
+            "infinite": true,
+            "slidesToShow": 5,
+            "slidesToScroll": 1,
+            "autoplay": true,
+            "speed": 6000,
+            "autoplaySpeed": 3500,
+            "cssEase": "linear",
+            "centerPadding": '-200px',
+            "centerMode": true,
+            "touchMove": true,
+
+            "responsive": [
+                {
+                    "breakpoint": 991,
+                    "settings": {
+                        "slidesToShow": 3,
+                        "centerPadding": '-50px',
+                    }
+                },
+                {
+                    "breakpoint": 767,
+                    "settings": {
+                        "slidesToShow": 2,
+                        "centerPadding": '-25px',
+                    }
+                },
+                {
+                    "breakpoint": 600,
+                    "settings": {
+                        "slidesToShow": 2,
+                        "centerPadding": '-25px',
+                    }
+                },
+                {
+                    "breakpoint": 520,
+                    "settings": {
+                        "slidesToShow": 2,
+                        "centerPadding": '-100px',
+                    }
+                }
+            ]
+
+        },
+      }
+    },
+
 }
 </script>
 
@@ -35,6 +155,7 @@ export default {
 
 @import 'resources/sass/front/_variables.scss';
 
+/* Jumbotron */
 .herobanner{
   background-image: url('/assets/img/banner.jpg');
   background-position: center;
@@ -48,6 +169,41 @@ h1{
 
 .lead{
   color: white;
+}
+
+/* Categories and Restaurants */
+
+/* Categories */
+.categories{
+    margin-top: 75px;
+    margin-bottom: 75px;
+}
+
+.cat-title h1{
+    color: black;
+}
+
+.cat-title p{
+    font-size: 1.125rem;
+    color: black;
+}
+
+.cat-cards{
+    height: 300px;
+    position: relative;
+
+    img{
+        height: 100%;
+    }
+
+    p{
+        position: absolute;
+        bottom: 10px;
+        color: white;
+        left: 25px;
+        font-weight: bold;
+        font-size: 1.2rem;
+    }
 }
 
 </style>
