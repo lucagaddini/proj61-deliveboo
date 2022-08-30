cec67a186010e63577f961e6e58d98600180369e
Luca Gaddini <90861515+lucagad@users.noreply.github.com>
2022-08-30 11:02:40 +0200

    Modificati Title Back & Front

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'DeliveBoo') }}</title>
+    <title>DeliveBoo | Gestisci il tuo ristorante</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/back.js') }}" defer></script>
