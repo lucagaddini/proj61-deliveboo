645bf1f94153a80b3c75e974bc88c68be5101a7f
Amanjit313 <amanjitsingh313@gmail.com>
2022-08-31 17:46:51 +0200

    jumbo quasi sistemato, da finire

--- resources/js/components/partials/HeroComp.vue
+++ resources/js/components/partials/HeroComp.vue
@@ -5,22 +5,23 @@
     <!-- Jumbotron -->
     <section class="herobanner">
         <div class="container col-xxl-8 pr-5 px-sm-4 py-5">
-            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+            <div class="row align-items-center g-5 py-5">
 
-                <div class="col-10 col-sm-8 col-lg-6">
+                <div class="col-12 col-sm-6">
+                    <h1 class="display-5 fw-bold lh-1 mb-3">Il cibo che ami a Domicilio</h1>
+                    <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate distinctio repudiandae perferendis quibusdam voluptas. Veritatis, enim rerum. Hic consectetur incidunt impedit, alias ratione tenetur ex praesentium nisi culpa vero aliquid?</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+
+                        <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Inserisci il tipo di piatto" aria-label="Search">
+                        <button class="btn btn_custom_secondary" type="submit">Trova</button>
+                        </form>
+
+                    </div>
                 </div>
 
-                <div class="col-lg-6">
-                <h1 class="display-5 fw-bold lh-1 mb-3">Il cibo che ami a Domicilio</h1>
-                <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate distinctio repudiandae perferendis quibusdam voluptas. Veritatis, enim rerum. Hic consectetur incidunt impedit, alias ratione tenetur ex praesentium nisi culpa vero aliquid?</p>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-
-                    <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Inserisci il tipo di piatto" aria-label="Search">
-                    <button class="btn btn_custom_secondary" type="submit">Trova</button>
-                    </form>
-
-                </div>
+                <div class="prova col-12 col-sm-6 d-none d-sm-block">
+                    <img src="/assets/img/logo-hero.png">
                 </div>
 
             </div>
@@ -46,10 +47,11 @@ export default {
 
 /* Jumbotron */
 .herobanner{
-  background-image: url('/assets/img/banner.jpg');
+/*   background-image: url('/assets/img/banner.jpg');
   background-position: center;
   background-size: cover;
-  background-repeat: no-repeat;
+  background-repeat: no-repeat; */
+  background-color: $fourth-color;
 }
 
 h1{
