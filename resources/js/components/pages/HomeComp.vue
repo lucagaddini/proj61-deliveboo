ed61ea58c3a1a82eb789c02b5e0942770088491c
Amanjit313 <amanjitsingh313@gmail.com>
2022-08-31 10:17:50 +0200

    popular categories sistemato, spostati le cat. e res. nel home comp

--- resources/js/components/pages/HomeComp.vue
+++ resources/js/components/pages/HomeComp.vue
@@ -1,22 +1,202 @@
 <template>
   <div>
+
+    <!-- Jumbotron -->
     <HeroComp />
+    <!-- /Jumbotron -->
+
+    <!-- Categories and Restaurants -->
+    <section>
+
+        <!-- Categories -->
+        <div class="categories container">
+
+            <!-- Cat. Title -->
+            <div class="cat-title text-center">
+                <h1>Popular Categories</h1>
+                <p>Lorem ipsum dolor sit amet, adipisicing!</p>
+            </div>
+            <!-- /Cat. Title -->
+
+            <VueSlickCarousel v-bind='settings'>
+
+            <!-- Cat. Cards -->
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p> Pizza</p>
+            </div>
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p>Pizza</p>
+            </div>
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p>Pizza</p>
+            </div>
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p>Pizza</p>
+            </div>
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p>Pizza</p>
+            </div>
+            <div class="cat-cards">
+                <img src="images/burghers-0.jpg">
+                <p>Pizza</p>
+            </div>
+            <!-- /Cat. Cards -->
+
+            </VueSlickCarousel>
+
+        </div>
+        <!-- /Categories -->
+
+        <hr>
+
+        <!-- Restaurants -->
+        <div class="restaurants container">
+
+            <!-- Res. Title -->
+            <div class="res-title">
+                <h2>Top Rated Restaurants</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti?</p>
+            </div>
+            <!-- /Res. Title -->
+
+            <!-- Res. Cards -->
+            <div class="res-cards">
+
+            </div>
+            <!-- /Res. Cards -->
+
+        </div>
+        <!-- /Restaurants -->
+
+    </section>
+    <!-- /Categories and Restaurants -->
+
   </div>
 </template>
 
 <script>
-import HeroComp from "../partials/HeroComp.vue"
+
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+import HeroComp from "../partials/HeroComp.vue";
 
 export default {
     name: 'HomeComp',
     components:{
-        HeroComp
-    }
+        HeroComp,
+        VueSlickCarousel
+    },
+
+    data() {
+      return {
+        settings: {
+            "dots": true,
+            "infinite": true,
+            "initialSlide": 0,
+            "speed": 500,
+            "slidesToShow": 4,
+            "slidesToScroll": 1,
+            "swipeToSlide": true,
+            "centerMode": true,
+            "centerPadding": "-50px",
+            "touchMove": true,
+
+            "responsive": [
+                {
+                    "breakpoint": 991,
+                    "settings": {
+                        "slidesToShow": 3,
+                        "centerPadding": '-50px',
+                    }
+                },
+                {
+                    "breakpoint": 767,
+                    "settings": {
+                        "slidesToShow": 2,
+                        "centerPadding": '-25px',
+                    }
+                },
+                {
+                    "breakpoint": 600,
+                    "settings": {
+                        "slidesToShow": 2,
+                        "centerPadding": '-25px',
+                    }
+                },
+                {
+                    "breakpoint": 520,
+                    "settings": {
+                        "slidesToShow": 2,
+                        "centerPadding": '-100px',
+                    }
+                }
+            ]
+
+        },
+      }
+    },
 }
 
 
 </script>
 
 <style lang='scss' scoped>
+
+/* Categories and Restaurants */
+
+/* Categories */
+.categories{
+    margin-top: 75px;
+    margin-bottom: 75px;
+}
+
+.cat-title h1{
+    color: black;
+}
+
+.cat-title p{
+    font-size: 1.125rem;
+    color: black;
+}
+
+.cat-cards{
+    height: 300px;
+    position: relative;
+
+    img{
+        height: 100%;
+    }
+
+    p{
+        position: absolute;
+        bottom: 10px;
+        color: white;
+        left: 25px;
+        font-weight: bold;
+        font-size: 1.2rem;
+    }
+}
+
+/* Restaurants */
+.restaurants{
+    margin-top: 75px;
+    margin-bottom: 75px;
+}
+
+.res-title h2{
+    color: black;
+}
+
+.res-title p{
+    font-size: 1.125rem;
+    color: black;
+}
+
 
 </style>
--- resources/js/components/partials/HeroComp.vue
+++ resources/js/components/partials/HeroComp.vue
@@ -1,10 +1,10 @@
 <template>
 
-<div class="home">
+  <div class="home">
 
     <!-- Jumbotron -->
     <section class="herobanner">
-        <div class="container col-xxl-8 px-4 py-5">
+        <div class="container col-xxl-8 pr-5 px-sm-4 py-5">
             <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
 
                 <div class="col-10 col-sm-8 col-lg-6">
@@ -28,144 +28,16 @@
     </section>
     <!-- /Jumbotron -->
 
-    <!-- Categories and Restaurants -->
-    <section>
-
-        <!-- Categories -->
-        <div class="categories container">
-
-            <!-- Cat. Title -->
-            <div class="cat-title text-center">
-                <h1>Popular Categories</h1>
-                <p>Lorem ipsum dolor sit amet, adipisicing!</p>
-            </div>
-            <!-- /Cat. Title -->
-
-            <VueSlickCarousel v-bind='settings'>
-
-            <!-- Cat. Cards -->
-            <div class="cat-cards">
-                <img src="images/burghers-0.jpg">
-                <p> Pizza</p>
-            </div>
-            <div class="cat-cards">
-                <img src="images/burghers-0.jpg">
-                <p>Pizza</p>
-            </div>
-            <div class="cat-cards">
-                <img src="images/burghers-0.jpg">
-                <p>Pizza</p>
-            </div>
-            <div class="cat-cards">
-                <img src="images/burghers-0.jpg">
-                <p>Pizza</p>
-            </div>
-            <div class="cat-cards">
-                <img src="images/burghers-0.jpg">
-                <p>Pizza</p>
-            </div>
-            <div class="cat-cards">
-                <img src="images/burghers-0.jpg">
-                <p>Pizza</p>
-            </div>
-            <!-- /Cat. Cards -->
-
-            </VueSlickCarousel>
-
-        </div>
-        <!-- /Categories -->
-
-        <hr>
-
-        <!-- Restaurants -->
-        <div class="restaurants container">
-
-            <!-- Res. Title -->
-            <div class="res-title">
-                <h2>Top Rated Restaurants</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti?</p>
-            </div>
-            <!-- /Res. Title -->
-
-            <!-- Res. Cards -->
-            <div class="res-cards">
-
-            </div>
-            <!-- /Res. Cards -->
-
-        </div>
-        <!-- /Restaurants -->
-
-    </section>
-    <!-- /Categories and Restaurants -->
-
   </div>
 
 </template>
 
 <script>
 
-  import VueSlickCarousel from 'vue-slick-carousel';
-  import 'vue-slick-carousel/dist/vue-slick-carousel.css';
-  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
-
 export default {
-    name: 'HeroComp',
-    components: {
-        VueSlickCarousel
-    },
-
-    data() {
-      return {
-        settings: {
-            "arrows": true,
-            "infinite": true,
-            "slidesToShow": 5,
-            "slidesToScroll": 1,
-            "autoplay": true,
-            "speed": 6000,
-            "autoplaySpeed": 3500,
-            "cssEase": "linear",
-            "centerPadding": '-200px',
-            "centerMode": true,
-            "touchMove": true,
-
-            "responsive": [
-                {
-                    "breakpoint": 991,
-                    "settings": {
-                        "slidesToShow": 3,
-                        "centerPadding": '-50px',
-                    }
-                },
-                {
-                    "breakpoint": 767,
-                    "settings": {
-                        "slidesToShow": 2,
-                        "centerPadding": '-25px',
-                    }
-                },
-                {
-                    "breakpoint": 600,
-                    "settings": {
-                        "slidesToShow": 2,
-                        "centerPadding": '-25px',
-                    }
-                },
-                {
-                    "breakpoint": 520,
-                    "settings": {
-                        "slidesToShow": 2,
-                        "centerPadding": '-100px',
-                    }
-                }
-            ]
-
-        },
-      }
-    },
-
+    name: 'HeroComp'
 }
+
 </script>
 
 <style lang="scss" scoped>
@@ -192,56 +64,6 @@ h1{
 
 .lead{
   color: white;
-}
-
-/* Categories and Restaurants */
-
-/* Categories */
-.categories{
-    margin-top: 75px;
-    margin-bottom: 75px;
-}
-
-.cat-title h1{
-    color: black;
-}
-
-.cat-title p{
-    font-size: 1.125rem;
-    color: black;
-}
-
-.cat-cards{
-    height: 300px;
-    position: relative;
-
-    img{
-        height: 100%;
-    }
-
-    p{
-        position: absolute;
-        bottom: 10px;
-        color: white;
-        left: 25px;
-        font-weight: bold;
-        font-size: 1.2rem;
-    }
-}
-
-/* Restaurants */
-.restaurants{
-    margin-top: 75px;
-    margin-bottom: 75px;
-}
-
-.res-title h2{
-    color: black;
-}
-
-.res-title p{
-    font-size: 1.125rem;
-    color: black;
 }
 
 </style>
