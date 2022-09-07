186802b485d861988bc8e2bea11269d5da51ef4e
iliescumichele <iliescumichele@gmail.com>
2022-09-08 01:52:19 +0200

    2ore di imprecazioni, vue validate non va... ora provo vee validate

--- database/seeds/OrdersTableSeeder.php
+++ database/seeds/OrdersTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Order;
-use App\Item;
+//use App\Item;
 use Illuminate\Support\Str;
 
 use Faker\Generator as Faker;
@@ -21,7 +21,9 @@ class OrdersTableSeeder extends Seeder
         //     'name', 'surname', 'email','address','telephone_number','total',
         // ];
 
-        for($i = 0; $i < 150 ; $i++){
+        $max = 6;
+
+        for($i = 0; $i < $max ; $i++){
             $new_order = new Order();
             $new_order->name = $faker->firstNameMale();
             $new_order->surname = $faker->lastName();
@@ -31,8 +33,74 @@ class OrdersTableSeeder extends Seeder
             $new_order->total = $faker->numberBetween(8, 104);
             $new_order->save();
 
-            $item_id = Item::inRandomOrder()->first()->id;
-            $new_order -> items() -> attach($item_id, array("quantity" =>  $faker->randomDigit()));
+            // $item_id = Item::inOrder()->first()->id;
+            // $item_id = Item::orderBy('id', 'asc')->first();
+            // $item_id = Item::orderByDesc('id')->get();
+            //if($i >= 0 && $i < 10){
+                $item_id = $faker->randomElements(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'], 4);
+
+            //}
+            $new_order -> items() -> attach($item_id, array("quantity" =>  4));
+        }
+
+        for($i = 0; $i < $max ; $i++){
+            $new_order = new Order();
+            $new_order->name = $faker->firstNameMale();
+            $new_order->surname = $faker->lastName();
+            $new_order->email = $faker->email();
+            $new_order->address = $faker->address();
+            $new_order->telephone_number = $faker->e164PhoneNumber();
+            $new_order->total = $faker->numberBetween(8, 104);
+            $new_order->save();
+
+            $item_id = $faker->randomElements(['14', '15', '16', '17', '18', '19', '20'], 4);
+
+            $new_order -> items() -> attach($item_id, array("quantity" =>  4));
+        }
+
+        for($i = 0; $i < $max ; $i++){
+            $new_order = new Order();
+            $new_order->name = $faker->firstNameMale();
+            $new_order->surname = $faker->lastName();
+            $new_order->email = $faker->email();
+            $new_order->address = $faker->address();
+            $new_order->telephone_number = $faker->e164PhoneNumber();
+            $new_order->total = $faker->numberBetween(8, 104);
+            $new_order->save();
+
+            $item_id = $faker->randomElements([21, 22, 23, 24, 25, 26, 27, 28 ,29], 4);
+
+            $new_order -> items() -> attach($item_id, array("quantity" =>  4));
+        }
+
+        for($i = 0; $i < $max ; $i++){
+            $new_order = new Order();
+            $new_order->name = $faker->firstNameMale();
+            $new_order->surname = $faker->lastName();
+            $new_order->email = $faker->email();
+            $new_order->address = $faker->address();
+            $new_order->telephone_number = $faker->e164PhoneNumber();
+            $new_order->total = $faker->numberBetween(8, 104);
+            $new_order->save();
+
+            $item_id = $faker->randomElements([30, 31, 32, 33, 34, 35, 36], 4);
+
+            $new_order -> items() -> attach($item_id, array("quantity" =>  4));
+        }
+
+        for($i = 0; $i < $max ; $i++){
+            $new_order = new Order();
+            $new_order->name = $faker->firstNameMale();
+            $new_order->surname = $faker->lastName();
+            $new_order->email = $faker->email();
+            $new_order->address = $faker->address();
+            $new_order->telephone_number = $faker->e164PhoneNumber();
+            $new_order->total = $faker->numberBetween(8, 104);
+            $new_order->save();
+
+            $item_id = $faker->randomElements([33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46], 4);
+
+            $new_order -> items() -> attach($item_id, array("quantity" =>  4));
         }
 
     }
